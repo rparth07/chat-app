33638f96eff68ab1ba87ffc3f759aa5b86243604
parth <pmr642001@gmail.com>
2021-02-08 17:05:40 +0530

    Building Out Chat Console

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import { Grid } from "semantic-ui-react";
 import "./App.css";
+import { connect } from "react-redux";
 
-class App extends Component {
-  render() {
-    return <div>APP</div>;
-  }
-}
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
 
-export default App;
+const App = ({ currentUser }) => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel currentUser={currentUser} />
+
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
+
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+export default connect(mapStateToProps)(App);
